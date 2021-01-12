908cd6f1f5cbe75597b1e355a15325c454b496b6
Alexandr Petrenko <alexandr.petrenko.fe@gmail.com>
2021-01-12 22:21:57 +0200

    Updates

---
+++ precache-manifest.9f50c6e99d107ec52600100917e10902.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d39df23aad05dd4298c5b98ab1bb1cb8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61e7aeb5a2e3db6ff37f",
+    "url": "./static/js/2.d1ce54ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d1ce54ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d923f89d72b3e565e4f7",
+    "url": "./static/js/main.79d5feb2.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
